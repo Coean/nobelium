3419d11fa682dd174c5125672525f7f269ec2864
Devpan <panhoucheng@gmail.com>
2024-05-07 11:06:48 +0800

    Update description in blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Devpan',
   email: 'admin@avj.asia',
   link: 'https://avj.asia',
-  description: '效率最高的学习就是输出',
+  description: 'Inspiration is Perishable, Now or Never.', // 效率最高的学习就是输出
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
